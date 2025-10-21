1f2484e698c1c76c764cb11a538a72a37e18da2a
edsonbastos2 <edsonbastos2@hotmail.com>
2025-10-20 22:04:53 -0300

    feat:adiciona relacionamento manyTomany na table product, criado table tags, script para  inserir dados na base

--- src/main/java/com/ecommerce/entities/ProductEntity.java
+++ src/main/java/com/ecommerce/entities/ProductEntity.java
@@ -3,6 +3,7 @@ package com.ecommerce.entities;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_products")
@@ -18,6 +19,15 @@ public class ProductEntity {
 
     @Column(name = "price")
     private BigDecimal price;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tb_products_tags",
+            uniqueConstraints = @UniqueConstraint(columnNames = {"product_id", "tag_id"}),
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private List<TagsEntity> tags;
 
     public ProductEntity() {
     }
@@ -44,5 +54,13 @@ public class ProductEntity {
 
     public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+
+    public List<TagsEntity> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<TagsEntity> tags) {
+        this.tags = tags;
     }
 }
---
+++ src/main/java/com/ecommerce/entities/TagsEntity.java
@@ -0,0 +1,35 @@
+package com.ecommerce.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_tags")
+public class TagsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long id;
+
+    @Column(name = "tag_name")
+    private String name;
+
+    public TagsEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
